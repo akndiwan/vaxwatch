a3cda3e41f41a59c924ec2b6b4b2221336090cd8
Ameya Diwan <akndiwan@Ameyas-MacBook-Pro.local>
2020-06-14 00:53:29 -0400

    final

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import SnowballStemmer
-#nltk.download('wordnet')
+nltk.download('wordnet')
 from nltk.stem.wordnet import WordNetLemmatizer
-#nltk.download('stopwords')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 import locale
@@ -23,13 +23,17 @@ from PIL import Image
 from nltk.tag import StanfordNERTagger
 import os
 import nltk
+from collections import Counter
+import spacy
+import en_core_web_sm
+
 
 
 app = Flask(__name__)
 
 API_KEY="ecbb55b0f6a941a2acfe909ee880eb55"
 topic="covid vaccine trial"
-yestdate=(datetime.today() - timedelta(1)).strftime('%Y-%m-%d')
+yestdate=(datetime.today() - timedelta(2)).strftime('%Y-%m-%d')
 
 url = ('http://newsapi.org/v2/everything?q='+topic+'&from='+yestdate+'&sortBy=publishedAt&apiKey='+API_KEY+'&language=en&pageSize=100&page=1&sortBy=popularity&language=en')
 response = requests.get(url)
@@ -102,79 +106,55 @@ text=str(newsdf["title"].values)
 
 def preprocessor(text):
     if type(text) == str:
+        text = text.lower()
         text = re.sub('<[^>]*>', ' ', text)
-        text = re.sub('[\W]+', ' ', text)
+        #text = re.sub('[\W]+', ' ', text)
         text = re.sub('xa0', '', text)
-        text=text.replace('Inc', '')
-        text=text.replace('Inc.', '')
-        text=text.replace('Times', '')
-        text=text.replace('News', '')
+        text = text.lower()
+        text= re.sub("'", "", text)
+        text= re.sub(",", "", text)
+        text = re.sub(r"\d", "", text)
+        text = re.sub(r"[,@\'?\.$%_]", "", text, flags=re.I)
+        text = text.replace("''", "")
+        text = text.replace('"', "")
         return text
-    
-text=preprocessor(text)
 
-# set java path
-java_path ='file:///Library/Java/JavaVirtualMachines/jdk-13.0.2.jdk/Contents/Home/'
-os.environ['JAVAHOME'] = java_path
-# initialize NER tagger
-sn = StanfordNERTagger('/Users/akndiwan/StanfordNLP/stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz',
-                       path_to_jar='/Users/akndiwan/StanfordNLP/stanford-ner-4.0.0/stanford-ner.jar')
-# tag named entities
-words = nltk.word_tokenize(text)
-tags=(sn.tag(words))
-# extract all named entities
-named_entities = []
-temp_named_entity=None
-temp_entity_name = ''
-for term, tag in tags:
-    if tag != 'O':
-        temp_entity_name = ' '.join([temp_entity_name, term]).strip()
-        temp_named_entity = (temp_entity_name, tag)
-    else:
-        if temp_named_entity:
-            named_entities.append(temp_named_entity)
-            temp_entity_name = ''
-            temp_named_entity = None
-#named_entities = list(set(named_entities))
-entity_frame = pd.DataFrame(named_entities, 
-                            columns=['Entity Name', 'Entity Type'])
-# view top entities and types
-top_entities = (entity_frame.groupby(by=['Entity Name', 'Entity Type'])
-                           .size()
-                           .sort_values(ascending=False)
-                           .reset_index().rename(columns={0 : 'Frequency'}))
-top_entities = (entity_frame.groupby(by=['Entity Name', 'Entity Type'])
-                           .size()
-                           .sort_values(ascending=False)
-                           .reset_index().rename(columns={0 : 'Frequency'}))
-ImpEntities=top_entities
-#[(top_entities["Entity Type"]=="ORGANIZATION") | (top_entities["Entity Type"]=="PERSON")]
-text=str(ImpEntities["Entity Name"].values)
-text=text.replace("'", "")
-text=re.sub("(\\d|\\W)+"," ",text)
+nlp = spacy.load("en_core_web_sm")
+
+newsdf['clean_titles']=newsdf.title.apply(preprocessor)
+tokens = nlp(''.join(str(newsdf.clean_titles.tolist())))
+items = [x.text for x in tokens.ents]
+entities=Counter(items).most_common(50)
+
+df_entities = pd.DataFrame(entities, columns =['text', 'count'])
+df_entities=df_entities[df_entities["count"]>1]
+
+d = {}
+for a, x in df_entities.values:
+    d[a] = x
+    
+import numpy as np
+from PIL import Image
+import nltk
+
+from wordcloud import ImageColorGenerator
 
 stop_words = nltk.corpus.stopwords.words('english')
-stem_desc=[]
-split_text = text.split()
-#Lemmatisation
-lem = WordNetLemmatizer()
-split_text = [lem.lemmatize(word) for word in split_text if not word in stop_words] 
-split_text = " ".join(split_text)
-#stem_desc.append(split_text)
-split_text=split_text.lower()
-newStopWords = ["university","times","reuters","inc","international","associated","press","organization","health","house","hill","concerns","rapid,""world","buy","pharma",'covid','coronavirus','sciences','hindustan','guide','institute','new','york','state','bbc','program','pharmaceuticals','innovations','cnn','grants','epidemic','respiratory','paycheck','protection','cnet','finance','commerce','bank','healthcare','systems','vaccine','right','top','preventative','shows','college','stock','children','promise','power','yearly','shows','global','republic','research','fate','market','podcast','transcript','city',"serum","journal","clinic","candidate","antibody","start","initiative","plans","report","engineer","phase","genetic","business","stocks","response","cocktail","reopening"]
+
+newStopWords = ["university","times","reuters","inc","international","organization","health","house","world","buy","pharma",'covid','sciences','hindustan','guide','institute','state','bbc','program','pharmaceuticals','innovations','cnn','grants','epidemic','respiratory','paycheck','protection','vaccine',"covid",'coronavirus',"today","daily","million","billion","first","second","january","february","march","april","may","june","july","august","september","october","november","december"]
 stop_words.extend(newStopWords)
-_mask = np.array(Image.open("static/images/covidcloud.jpg").convert('RGB'))
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+_mask = np.array(Image.open("/Users/akndiwan/VaxNews/static/images/covidcloud.jpg").convert('RGB'))
 _mask[_mask==0]=255
-#set the word cloud parameters
-wordcloud = WordCloud( background_color="white",width = 200,height =200, stopwords = stop_words, max_words = 100000,min_font_size = 5, mask=_mask, random_state=42,mode="RGBA", max_font_size=50).generate(split_text)
-#plot the word cloud
+
+wordcloud = WordCloud(background_color="white",width = 200,height = 200, stopwords = stop_words, max_words = 50,min_font_size = 3, mask=_mask, random_state=42).generate_from_frequencies(frequencies=d)
 fig = plt.figure(figsize = (20,10))
 image_colors = ImageColorGenerator(_mask)
 plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation = "bilinear")
 plt.axis('off')
 plt.tight_layout(pad=0)
-plt.savefig("static/images/VaxWC.png",format="png")
+plt.savefig("/Users/akndiwan/VaxNews/static/images/VaxWC.png",format="png")
 #plt.show()
 
 
